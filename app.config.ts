c6d6001fdcf06632e7c5ca3a947836538c029534
Jörgen Granefelt <jorgen.granefelt@nimbulus.se>
2026-05-08 08:44:26 +0200

    Versionsuppdatering

--- app.config.ts
+++ app.config.ts
@@ -4,7 +4,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
   name: 'Kontrollen',
   slug: 'ol-kollen',
-  version: '1.0.0',
+  version: '1.2.0',
   orientation: 'portrait',
   icon: './assets/icon1024.png',
   userInterfaceStyle: 'light',
